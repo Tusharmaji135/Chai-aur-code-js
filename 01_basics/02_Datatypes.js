58419970e75f5378ab0bd650a9df990e2902e19c
Tushar Maji <135tusharmaji@gmail.com>
2024-06-20 20:20:54 +0530

    Data type

--- 01_basics/02_Datatypes.js
+++ 01_basics/02_Datatypes.js
@@ -1,6 +1,29 @@
-"use strict";//treat all as new version
-// alert("hello") //we use node not browser
+"use strict"; // treat all JS code as newer version
 
-console.log(44+88);
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
 
 
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
