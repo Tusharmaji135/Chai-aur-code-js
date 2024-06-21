7a8a4bd666182ba9bbab12564d48ea4edec83dc4
Tusharmaji135 <135tusharmaji@gmail.com>
2024-06-21 09:42:47 +0530

    all done

---
+++ 01_basics/03_DatatypeConver.js
@@ -0,0 +1,18 @@
+// let score = "33ac";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let a = "tushar"
+// let b = Boolean(a)
+// console.log(typeof b);
+// console.log(b);
+
+let someNum =33
+let str = String(someNum)
+console.log(typeof str);
+console.log(str);
+
