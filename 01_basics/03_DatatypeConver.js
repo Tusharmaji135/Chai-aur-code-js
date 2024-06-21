e349bbd290b179b23e3f3378914caf47a0835653
Tusharmaji135 <135tusharmaji@gmail.com>
2024-06-21 10:46:32 +0530

    first commit

---
+++ 01_basics/03_DatatypeConver.js
@@ -0,0 +1,39 @@
+// let score = "33ac";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let a = "tushar"
+// let b = Boolean(a)
+// console.log(typeof b);
+// console.log(b);
+
+let someNum =33
+let str = String(someNum)
+// console.log(typeof str);
+// console.log(str);
+
+//***************operations********************
+
+let value =3
+let negValue = -value
+// console.log(negValue);
+
+// console.log('2'+2+2);
+
+// console.log(+true);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
---
+++ 01_basics/datatypeSummary.js
@@ -0,0 +1,40 @@
+//  Primitive
+
+//  7 types : String, Number, Booleen, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+console.table(myObj);
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+console.log(BigInt);
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
